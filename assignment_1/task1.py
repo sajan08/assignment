21a5a32e11eb66123d310e86e55409fc8ee0ba4d
sajanntipl <you@example.com>
2025-03-05 10:38:09 +0530

    assignement 1 complete

--- assignment_1/task1.py
+++ assignment_1/task1.py
@@ -1,5 +1,5 @@
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
 
 c = (a) + (b)
 print("Addition: ", c)
--- assignment_1/task2.py
+++ assignment_1/task2.py
@@ -1,4 +1,4 @@
-first_name  = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
+first_name  = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
 
 print("Hello, " + first_name + " " + last_name + "! Welcome to Python Programming")
