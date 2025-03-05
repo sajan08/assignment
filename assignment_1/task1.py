9819e62911107a808c44873434a5caa9ea509866
sajanntipl <you@example.com>
2025-03-05 10:36:19 +0530

    assignement 1 complete

---
+++ assignment_1/task1.py
@@ -0,0 +1,14 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+c = (a) + (b)
+print("Addition: ", c)
+
+c = a - b
+print("Subtraction: ", c)
+
+c = a * b
+print("Multiplication: ", c)
+
+c = a / b
+print("Division: ", c)
